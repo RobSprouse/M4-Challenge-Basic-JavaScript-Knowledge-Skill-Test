7143ffee511bb4895f22f231f52b1bec43e36903
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-13 01:35:47 -0400

    stopping point

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,34 +1,7 @@
 let questionList = [
     {
         question: "This is the first question.",
-        // COMMENT: Correct answer is [0] in the array.
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the second question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the third question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the fourth question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the fifth question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the six question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the seventh question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the eight question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the ninth question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
-    },{
-        question: "This is the tenth question"
-        answers: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"]
+        multipleChoice: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"],
+        answer: 0
     }
 ];
