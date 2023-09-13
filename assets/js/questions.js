55d1e3e72a0ee8f7f3426a4d8f91d01345b6b94a
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-13 15:58:46 -0400

    cleanup formatting

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,102 @@
 let questionList = [
-    {
-        question: "This is the first question.",
-        multipleChoice: ["Correct answer.", "wrong answer", "wrong answer", "wrong answer"],
-        answer: 0
-    }
+  {
+    question: "This is the first question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the second question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the third question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the fourth question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the fifth question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the sixth question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the seventh question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the eighth question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the ninth question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
+  {
+    question: "This is the tenth question.",
+    multipleChoice: [
+      "Correct answer.",
+      "wrong answer",
+      "wrong answer",
+      "wrong answer",
+    ],
+    answer: 0,
+  },
 ];
