8da4d651606834c20441462a581a203046e4a7b1
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-14 11:04:59 -0400

    set up for adding criteria
    Asks questions and checks them properly. Needs code to meet personal and required criteria.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,13 @@
 console.log("JS is connected.");
 
-// TODO: assign objects: timerDiv, timer span, instructionsDiv, buttonDiv,
-// TODO:                 startTest button, questionsDiv, questions p, multipleChoiceAnswersDiv, multipleChoice list
-
-// let timerDiv = $(".timerDiv")
-// let timer = $("#timer")
-// let instructionsDiv = $(".instructionsDiv")
-// let buttonDiv = $(".buttonDiv")
-// let startTest = $("#startTest")
-// let questionsDiv = $(".questionsDiv")
-// let questions = $("#questions")
-// let multipleChoice = $("#multipleChoice")
 
 $(".timerDiv").hide();
 $(".questionsDiv").hide();
 $(".multipleChoiceAnswersDiv").hide();
+
+let timeLeft = 60;
+let questionAsked = [] // TODO:
+
 
 $("#startTest").click(function () {
   $(".instructionsDiv").hide();
@@ -22,4 +15,66 @@ $("#startTest").click(function () {
   $(".timerDiv").show();
   $(".questionsDiv").show();
   $(".multipleChoiceAnswersDiv").show();
+  displayQuestion(); 
 });
+
+
+function displayQuestion() {
+  let timer = setInterval(function () {
+    if(timeLeft <= 0) {
+      clearInterval(timer);
+      alert("Time's up!");
+      // TODO: add code after time is up
+    } else {
+      $("#timer").text(timeLeft + " seconds remaining")
+    }
+    timeLeft -= 1;
+  }, 1000)
+
+    // TODO: make only ten questions
+    let questionIndex = Math.floor(Math.random() * questionList.length);
+  
+    let questionAsked = questionList[questionIndex];
+    
+    questionList.splice(questionIndex, 1)
+
+  $("#questions").text(questionAsked.question);
+
+  $("#multipleChoice").empty();
+
+  let choices = choicesShuffled(questionAsked.multipleChoice);
+
+  for (let i = 0; i < choices.length; i++) {
+    let li = $("<li></li>").text(choices[i]);
+    li.click(function () {
+      checkAnswer(questionAsked, $(this).text());
+    });
+    $("#multipleChoice").append(li);
+  }
+
+  questionList.splice(questionIndex, 1);
+}
+
+function checkAnswer(question, selectedChoice) {
+  if (selectedChoice !== question.answer) {
+    timeLeft -= 10;
+  }
+// TODO: add some type of notification when they get an answer right or wrong.
+
+  if (questionList.length > 0) {
+    displayQuestion(); 
+  } else {
+    alert("Quiz finished! Your score is:"); // TODO: figure out scoring system and add to highscores local storage
+  }
+}
+
+// COMMENT: Function to shuffle an array (Fisher-Yates shuffle)
+function choicesShuffled(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; 
+  }
+  return array;
+}
+
+// TODO: Set style changes to answers chosen, right, wrong
