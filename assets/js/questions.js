97cf82c1a0a767958ba5a3adb8df746cfb50b441
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-16 00:27:12 -0400

    Update questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,105 +3,100 @@ const questionList = [
     question: "question 1.",
     multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
     answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  },  {
-    question: "question 1.",
-    multipleChoice: ["1", "wrong answer", "wrong answer", "wrong answer"],
-    answer: "1",
-  }
+  },
+  {
+    question: "question 2.",
+    multipleChoice: ["2", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "2",
+  },
+  {
+    question: "question 3.",
+    multipleChoice: ["3", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "3",
+  },
+  {
+    question: "question 4.",
+    multipleChoice: ["4", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "4",
+  },
+  {
+    question: "question 5.",
+    multipleChoice: ["5", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "5",
+  },
+  {
+    question: "question 6.",
+    multipleChoice: ["6", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "6",
+  },
+  {
+    question: "question 7.",
+    multipleChoice: ["7", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "7",
+  },
+  {
+    question: "question 8.",
+    multipleChoice: ["8", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "8",
+  },
+  {
+    question: "question 9.",
+    multipleChoice: ["9", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "9",
+  },
+  {
+    question: "question 10.",
+    multipleChoice: ["10", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "10",
+  },
+  {
+    question: "question 11.",
+    multipleChoice: ["11", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "11",
+  },
+  {
+    question: "question 12.",
+    multipleChoice: ["12", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "12",
+  },
+  {
+    question: "question 13.",
+    multipleChoice: ["13", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "13",
+  },
+  {
+    question: "question 14.",
+    multipleChoice: ["14", "wrong answer", "wrong answer", "wrong answer"],
+    answer: "14",
+  },
+   {
+     question:"question15.",
+     multipleChoice:["15","Wrong Answer","Wrong Answer","Wrong Answer"],
+     Answer:"15"
+   },
+   {
+     question:"question16.",
+     multipleChoice:["16","Wrong Answer","Wrong Answer","Wrong Answer"],
+     Answer:"16"
+   },
+   {
+     question:"question17.",
+     multipleChoice:["17","Wrong Answer","Wrong Answer","Wrong Answer"],
+     Answer:"17"
+   },
+   {
+     question:"question18.",
+     multipleChoice:["18","Wrong Answer","Wrong Answer","Wrong Answer"],
+     Answer:"18"
+   },
+   {
+     question:"question19.",
+     multipleChoice:["19","Wrong Answer","Wrong Answer","Wrong Answer"],
+     Answer:"19"
+   },
+   {
+     question:"question20.",
+     multipleChoice:["20","Wrong Answer","Wrong Answer","Wrong Answer"],
+     Answer:"20"
+   }
 ];
